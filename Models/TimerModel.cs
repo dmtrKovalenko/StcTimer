7dd14acf7c2bd9a261262a468ec9af5d615b2c7c
Quint <dmtr.kovalenko@outlook.com>
2016-02-06 15:11:27 +0200

    Commit

---
+++ Models/TimerModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StcTimer.Models
+{
+    class TimerModel : INotifyPropertyChanged
+    {
+        public double _Progress;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+        }
+
+        public double Progress
+        {
+            get { return _Progress; }
+            set
+            {
+                if (_Progress != value)
+                {
+                    _Progress = value;
+                    OnPropertyChanged("Progress");
+                }
+            }
+        }
+
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StcTimer.Models;
+
+namespace StcTimer.ViewModels
+{
+    class MainWindowViewModel
+    {
+        public TimerModel TimerProperty { get; set; }
+        Timer timer = new Timer();
+       
+        public MainWindowViewModel()
+        {
+            TimerProperty = new TimerModel
+            { 
+                _Progress = Timer._progress
+            };
+  //          TimerProperty.PropertyChanged += TimerProperty_PropertyChanged;
+        }
+
+   /*     private void TimerProperty_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            TimerProperty = new TimerModel
+            {
+                _Progress = Timer._progress
+            };
+        }*/
+    }
+}
